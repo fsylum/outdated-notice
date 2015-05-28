92b4b37dac6edc3a42595ee51f79cc038bfa7e4c
Firdaus Zahari <fake@fsylum.net>
2015-05-28 09:08:34 +0800

    Working code for Part 2

---
+++ admin/partials/outdated-notice-admin-display.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://fsylum.net
+ * @since      1.0.0
+ *
+ * @package    Outdated_Notice
+ * @subpackage Outdated_Notice/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<div class="wrap">
+    <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+    <form action="options.php" method="post">
+        <?php
+            settings_fields( $this->plugin_name );
+            do_settings_sections( $this->plugin_name );
+            submit_button();
+        ?>
+    </form>
+</div>
---
+++ public/partials/outdated-notice-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       http://fsylum.net
+ * @since      1.0.0
+ *
+ * @package    Outdated_Notice
+ * @subpackage Outdated_Notice/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
